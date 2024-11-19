696c3bdf42168b721858ca24ef1f72f973c2f87c
GiorgioDordo <djole6789@gmail.com>
2024-11-19 18:09:58 +0100

    created layout view admin.create

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -4,10 +4,23 @@
 @section("page-title", "Create Project")
 
 @section("content")
-<main class="container">
-    <div class="row d-flex justify-content-center">
-        <h1>crea progetto
-        </h1>
+<main class="container d-flex justify-content-center">
+    <div class="card p-4">
+        <div class="card-title">
+            <h1>CREA PROGETTO</h1>
+        </div>
+        <div class="card-body">
+            <form action="" method="">
+                <div class="row">
+                    <div class="col">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </main>
 @endsection
